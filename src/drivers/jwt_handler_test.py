6cb1de353fd0954050c751002c93743810aa306a
Guilherme Jorge Hauck <78877899+Guihauck@users.noreply.github.com>
2025-11-17 11:26:35 -0300

    feat: Implementing JwtHandler

---
+++ src/drivers/jwt_handler_test.py
@@ -0,0 +1,17 @@
+from src.drivers.jwt_handler import JwtHandler
+
+def test_jwt_handler():
+    jwt_handler = JwtHandler()
+    body = {
+        "username": "guihauck98",
+        "password": "hauck_19998",
+        "description": "account"
+    }
+
+    token = jwt_handler.create_jwt_token(body)
+    token_informations = jwt_handler.decode_jwt_token(token)
+
+    assert token is not None
+    assert isinstance(token, str)
+    assert token_informations["username"] == body["username"]
+    assert token_informations["description"] == body["description"]
